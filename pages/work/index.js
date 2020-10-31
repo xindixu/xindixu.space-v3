239a0e41ead6ae6d5b685e03e1b9457129c94c91
Xindi Xu <xindixu@utexas.edu>
2020-10-31 03:48:18 -0500

    get content from contentful

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Box, Card, CardBody, CardFooter, Grid, Grommet, Text } from "grommet";
+import { getAllWorks } from "lib/contentful/work";
 
-const Work = (props) => {
+const Work = ({ works }) => {
+  console.log(works);
   return <div>Work page</div>;
 };
+
+export async function getStaticProps() {
+  const works = await getAllWorks();
+
+  // Next.js expects the props to be json stringify-able
+  // https://dev.to/ryyppy/reason-records-nextjs-undefined-and-getstaticprops-5d46
+  return {
+    props: JSON.parse(
+      JSON.stringify({
+        works,
+      })
+    ),
+  };
+}
 
 Work.propTypes = {};
 
