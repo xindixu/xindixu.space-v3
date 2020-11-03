4d1369d62842abf6d6946adaca4ae476e109a08f
Xindi Xu <xindixu@utexas.edu>
2020-11-02 22:33:20 -0600

    update header to include tags

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import { Box, Heading } from "grommet"
 import styled from "styled-components"
 import styleSettings from "lib/style-settings"
 
-const { pink, white, spacerXxl } = styleSettings
+const { pink, white, spacerXs, spacerSm, spacerXxl } = styleSettings
 
 const Image = styled(Box)`
   ${({ url, position = "center" }) => `
@@ -18,23 +18,38 @@ const Gradient = styled(Box)`
   background: radial-gradient(circle, ${white}00 60%, ${pink}66 100%);
 `
 
-const Text = styled(Box)`
+const Content = styled(Box)`
   padding: ${spacerXxl};
   background: radial-gradient(circle, ${white}55 60%, ${white} 100%);
 `
 
-const Header = React.forwardRef(({ name, background, full }, ref) => {
-  return (
-    <Image {...background} fill={full ? true : "horizontal"} ref={ref}>
-      <Gradient fill align="center" justify="center">
-        <Text>
-          <Heading color="black" size={full ? "xlarge" : "medium"}>
-            {name}
-          </Heading>
-        </Text>
-      </Gradient>
-    </Image>
-  )
-})
+const Label = styled(Box)`
+  background: ${pink};
+  margin: ${spacerXs} ${spacerSm};
+`
+
+const Header = React.forwardRef(({ name, background, full, labels }, ref) => (
+  <Image {...background} fill={full ? true : "horizontal"} ref={ref}>
+    <Gradient fill align="center" justify="center">
+      <Content>
+        <Heading
+          color="black"
+          size={full ? "xlarge" : "medium"}
+          textAlign="center"
+        >
+          {name}
+        </Heading>
+        <Box direction="row" wrap>
+          {labels &&
+            labels.map((label) => (
+              <Label key={label} pad={{ horizontal: "small" }} round>
+                {label}
+              </Label>
+            ))}
+        </Box>
+      </Content>
+    </Gradient>
+  </Image>
+))
 
 export default Header
