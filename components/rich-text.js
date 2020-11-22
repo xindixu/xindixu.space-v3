fdbbb3e0f27f7bdad7dcd9592dfff86c2ee2a3d0
Xindi Xu <xindixu@utexas.edu>
2020-11-22 13:55:17 -0600

    project detail: more custom styles

--- components/rich-text.js
+++ components/rich-text.js
@@ -1,19 +1,39 @@
 import React from "react"
+import styled from "styled-components"
 import { BLOCKS, MARKS } from "@contentful/rich-text-types"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { Paragraph, Heading } from "grommet"
+import styleSettings from "lib/style-settings"
 
-const Bold = ({ children }) => <span className="bold">{children}</span>
+const { pink, focus } = styleSettings
 
-const Text = ({ children }) => <p className="align-center">{children}</p>
+const Code = styled.code`
+  font-size: 87.5%;
+  color: ${focus};
+  word-break: break-word;
+`
+const ULList = styled.ul``
+const ListItem = styled.li`
+  &::marker {
+    color: ${pink};
+  }
+`
 
 const options = {
+  renderMark: {
+    [MARKS.CODE]: (text) => <Code>{text}</Code>,
+  },
   renderNode: {
-    [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+    [BLOCKS.PARAGRAPH]: (_, children) => <Paragraph fill>{children}</Paragraph>,
+    [BLOCKS.HEADING_1]: (_, children) => (
+      <Heading level={2}>{children}</Heading>
+    ),
+    [BLOCKS.UL_LIST]: (_, children) => <ULList>{children}</ULList>,
+    [BLOCKS.LIST_ITEM]: (_, children) => <ListItem>{children}</ListItem>,
   },
 }
 
-const RichText = ({ document }) => {
-  return documentToReactComponents(document, options)
-}
+const RichText = ({ document }) =>
+  console.log(document) || documentToReactComponents(document, options)
 
 export default RichText
