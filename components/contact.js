8986ffff724468a91d0509c22bed7767dab91171
Xindi Xu <xindixu@utexas.edu>
2020-11-25 01:36:28 -0600

    feat(contact): validation + use native form

--- components/contact.js
+++ components/contact.js
@@ -4,45 +4,57 @@ import { Box, Form, TextArea, TextInput, FormField, Button } from "grommet"
 import { send } from "lib/email"
 import Toast from "components/toast"
 
+const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 const Contact = () => {
-  const [form, setForm] = useState({})
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [toast, setToast] = useState({})
 
-  const onSubmit = () => {
+  const onSubmit = (value) => {
     setIsSubmitting(true)
-    send(form)
-      .then(() => {
-        setForm({})
+    send(value)
+      .then(() =>
         setToast({ mode: "ok", content: "Your email has been sent!" })
-      })
-      .catch(() => {
+      )
+      .catch(() =>
         setToast({
           mode: "error",
           content: "We couldn't send your email. Try again in a few minutes",
         })
-      })
+      )
       .finally(() => setIsSubmitting(false))
   }
   return (
     <>
-      <Form onChange={setForm}>
+      <Form
+        onSubmit={({ value }) => onSubmit(value)}
+        validate="blur"
+        disabled={isSubmitting}
+      >
         <Box gap="medium">
           <Box direction="row" gap="medium" fill>
-            <FormField name="name" label="Name" fill>
+            <FormField name="name" label="Name" fill required>
               <TextInput name="name" />
             </FormField>
-            <FormField name="email" label="Email" fill>
+            <FormField
+              name="email"
+              label="Email"
+              fill
+              required
+              validate={{
+                regexp: emailRegex,
+                message: "not a validate email address",
+                status: "error",
+              }}
+            >
               <TextInput name="email" />
             </FormField>
           </Box>
-          <FormField name="message" label="Message" fill>
+          <FormField name="message" label="Message" fill required>
             <TextArea name="message" size="medium" />
           </FormField>
           <Box direction="row" gap="medium">
             <Button
-              type="button"
-              onClick={onSubmit}
+              type="submit"
               disabled={isSubmitting}
               primary
               label="Send Email"
