f67ca4e786695d6fd920c5c36bc7eb4be30625d8
Xindi Xu <xindixu@utexas.edu>
2021-06-02 15:34:50 -0500

    feat(content): render embedded assets, image and video

--- components/rich-text.js
+++ components/rich-text.js
@@ -1,8 +1,10 @@
 import React from "react"
 import styled from "styled-components"
+import { get } from "lodash"
 import { BLOCKS, MARKS } from "@contentful/rich-text-types"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { Paragraph, Heading } from "grommet"
+import Image from "next/image"
 import styleSettings from "lib/style-settings"
 
 const { pink, focus } = styleSettings
@@ -18,6 +20,26 @@ const ListItem = styled.li`
     color: ${pink};
   }
 `
+const Video = styled.video`
+  display: block;
+  visibility: visible;
+  width: 100%;
+  height: auto;
+`
+
+const EmbeddedImage = ({ url, title, width, height }) => (
+  <Image
+    src={`https:${url}`}
+    layout="responsive"
+    width={width}
+    height={height}
+    alt={title}
+  />
+)
+
+const EmbeddedVideo = ({ url, title }) => (
+  <Video controls name={title} playsinline src={`https:${url}`} />
+)
 
 const options = {
   renderMark: {
@@ -30,6 +52,29 @@ const options = {
     ),
     [BLOCKS.UL_LIST]: (_, children) => <ULList>{children}</ULList>,
     [BLOCKS.LIST_ITEM]: (_, children) => <ListItem>{children}</ListItem>,
+    [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      const { title, file } = get(node, "data.target.fields")
+      const { url, contentType, details } = file
+
+      if (contentType.startsWith("image")) {
+        const { width, height } = details?.image || {}
+
+        return (
+          <EmbeddedImage
+            url={url}
+            width={width}
+            height={height}
+            title={title}
+          />
+        )
+      }
+
+      if (contentType.startsWith("video")) {
+        return <EmbeddedVideo url={url} title={title} />
+      }
+
+      return null
+    },
   },
 }
 
