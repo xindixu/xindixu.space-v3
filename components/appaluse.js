0a992b54c173e17fe440d456b177d1069f39d5f4
Xindi Xu <xindixu0@gmail.com>
2021-06-10 02:58:43 -0500

    feat(animation): add animation to applauses

--- components/appaluse.js
+++ components/appaluse.js
@@ -2,16 +2,17 @@ import React, { useState } from "react"
 import styled from "styled-components"
 import {
   Box,
+  Button,
   Card,
-  CardFooter,
   CardBody,
+  CardFooter,
   CardHeader,
   Heading,
+  Stack,
   Text,
-  Button,
 } from "grommet"
 import { Previous, Next } from "grommet-icons"
-// import { useCycle, motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import { format } from "date-fns"
 import { applauses } from "contents/applauses"
 
@@ -21,79 +22,123 @@ const IconButton = styled(Button)`
   border-radius: 50%;
 `
 
-const Appause = () => {
-  const { length } = applauses
-  const [index, setIndex] = useState(0)
-  const { message, createdAt, sender, likes } = applauses[index]
+const variants = {
+  enter: (direction) => ({
+    x: direction > 0 ? 100 : -100,
+    opacity: 0,
+  }),
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction) => ({
+    zIndex: 0,
+    x: direction < 0 ? 100 : -100,
+    opacity: 0,
+  }),
+}
 
-  return (
-    // <motion.div
-    //   animate={{
-    //     scale: [1, 2, 2, 1, 1],
-    //     rotate: [0, 0, 270, 270, 0],
-    //     borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-    //   }}
-    //   transition={{
-    //     duration: 2,
-    //     ease: "easeInOut",
-    //     times: [0, 0.2, 0.5, 0.8, 1],
-    //     loop: Infinity,
-    //     repeatDelay: 1,
-    //   }}
-    //   style={{ background: "red", borderRadius: 30, width: 150, height: 150 }}
-    // />
-    <Card height="medium" width="medium" gap="medium" pad="medium">
-      <CardHeader>
-        <Heading level={4} margin="none">
-          Applauses from my coworkers
-        </Heading>
-      </CardHeader>
-      <CardBody>&ldquo;{message}&rdquo;</CardBody>
-      <CardFooter direction="column" align="start">
-        <Box direction="row" justify="start">
-          <Text color="gray">&mdash;</Text>
-          <Text color="gray" margin={{ horizontal: "xsmall" }} size="small">
-            {sender.name}, {sender.jobTitle} <br />
-            {format(new Date(createdAt), SHORT_DATE_FORMAT)}
+const Base = ({
+  createdAt,
+  index,
+  length,
+  likes,
+  message,
+  paginate,
+  sender: { name, jobTitle },
+}) => (
+  <Card height="medium" width="medium" gap="medium" pad="medium">
+    <CardHeader>
+      <Heading level={4} margin="none">
+        Applauses from my coworkers
+      </Heading>
+    </CardHeader>
+    <CardBody>&ldquo;{message}&rdquo;</CardBody>
+    <CardFooter direction="column" align="start">
+      <Box direction="row" justify="start">
+        <Text color="gray">&mdash;</Text>
+        <Text color="gray" margin={{ horizontal: "xsmall" }} size="small">
+          {name}, {jobTitle} <br />
+          {format(new Date(createdAt), SHORT_DATE_FORMAT)}
+        </Text>
+      </Box>
+      <Box direction="row" align="center" justify="between" fill="horizontal">
+        <Box
+          border="all"
+          round="large"
+          pad={{ vertical: "xxsmall", horizontal: "small" }}
+        >
+          <Text size="small">
+            <span role="img" aria-label="Clapping hand">
+              👏
+            </span>{" "}
+            {likes}
           </Text>
         </Box>
-        <Box direction="row" align="center" justify="between" fill="horizontal">
-          <Box
-            border="all"
-            round="large"
-            pad={{ vertical: "xxsmall", horizontal: "small" }}
-          >
-            <Text size="small">
-              <span role="img" aria-label="Clapping hand">
-                👏
-              </span>{" "}
-              {likes}
-            </Text>
-          </Box>
-          <Box direction="row" align="center">
-            <IconButton
-              icon={<Previous size="small" />}
-              a11yTitle="previous"
-              round="full"
-              size="small"
-              disabled={index === 0}
-              onClick={() => setIndex((prevIndex) => prevIndex - 1)}
-            />
-            <Text margin={{ horizontal: "small" }} size="small">
-              {index + 1} of {length}
-            </Text>
-            <IconButton
-              icon={<Next size="small" />}
-              a11yTitle="next"
-              round="full"
-              size="small"
-              disabled={index === length - 1}
-              onClick={() => setIndex((prevIndex) => prevIndex + 1)}
-            />
-          </Box>
+        <Box direction="row" align="center">
+          <IconButton
+            icon={<Previous size="small" />}
+            a11yTitle="previous"
+            round="full"
+            size="small"
+            disabled={index === 0}
+            onClick={() => paginate(-1)}
+          />
+          <Text margin={{ horizontal: "small" }} size="small">
+            {index + 1} of {length}
+          </Text>
+          <IconButton
+            icon={<Next size="small" />}
+            a11yTitle="next"
+            round="full"
+            size="small"
+            disabled={index === length - 1}
+            onClick={() => paginate(1)}
+          />
         </Box>
-      </CardFooter>
-    </Card>
+      </Box>
+    </CardFooter>
+  </Card>
+)
+
+const Appause = () => {
+  const { length } = applauses
+
+  const [[page, direction], setPage] = useState([0, 0])
+  const index = page % length
+  const { message, createdAt, sender, likes } = applauses[index]
+
+  const paginate = (newDirection) =>
+    setPage([page + newDirection, newDirection])
+
+  return (
+    <AnimatePresence initial={false} custom={direction}>
+      <Stack anchor="top-right">
+        <motion.div
+          key={page}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 },
+          }}
+        >
+          <Base
+            createdAt={createdAt}
+            index={index}
+            length={length}
+            likes={likes}
+            message={message}
+            paginate={paginate}
+            sender={sender}
+          />
+        </motion.div>
+      </Stack>
+    </AnimatePresence>
   )
 }
 
