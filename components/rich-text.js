613b867a656b4b9b70f35a99ae4adec14b042ad5
Xindi Xu <xindixu0@gmail.com>
2021-06-15 23:47:30 -0500

    feat(project): render youtube and image collage

--- components/rich-text.js
+++ components/rich-text.js
@@ -1,13 +1,22 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import { kebabCase, get } from "lodash"
+import { Grid, Box, Text, Paragraph, Heading as BaseHeading } from "grommet"
 import { BLOCKS, MARKS } from "@contentful/rich-text-types"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import { Box, Text, Paragraph, Heading as BaseHeading } from "grommet"
-import Image from "next/image"
+import BaseImage from "next/image"
 import styleSettings from "lib/style-settings"
 
 const { pink, focus } = styleSettings
+
+const embeddedContentCss = css`
+  position: absolute;
+  border: none;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
 
 const Code = styled.code`
   font-size: 87.5%;
@@ -27,7 +36,7 @@ const Video = styled.video`
   height: auto;
 `
 
-const PdfWrapper = styled(Box)`
+const EmbedWrapper = styled(Box)`
   ${({ ratio }) => `
     padding-bottom: ${ratio}%;
   `}
@@ -35,13 +44,11 @@ const PdfWrapper = styled(Box)`
   max-width: 100%;
 `
 
-const Pdf = styled.embed`
-  position: absolute;
-  border: none;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+const IFrame = styled.iframe`
+  ${embeddedContentCss}
+`
+const Embed = styled.embed`
+  ${embeddedContentCss}
 `
 
 const Heading = styled(BaseHeading)`
@@ -50,22 +57,30 @@ const Heading = styled(BaseHeading)`
   pointer-events: none;
 `
 
+const Image = ({ file, alt }) => {
+  const { url, details } = file
+  const { width, height } = details?.image || {}
+
+  return (
+    <BaseImage
+      src={`https:${url}`}
+      layout="responsive"
+      width={width}
+      height={height}
+      alt={alt}
+    />
+  )
+}
 const Description = ({ description }) => (
   <Text color="dark-2" size="small" margin={{ bottom: "small" }}>
     {description}
   </Text>
 )
 
-const EmbeddedImage = ({ description, url, title, width, height }) => (
+const EmbeddedImage = ({ description, title, file }) => (
   <Box margin={{ bottom: "medium" }}>
     <Description description={description} />
-    <Image
-      src={`https:${url}`}
-      layout="responsive"
-      width={width}
-      height={height}
-      alt={title}
-    />
+    <Image file={file} alt={title} />
   </Box>
 )
 
@@ -76,12 +91,37 @@ const EmbeddedVideo = ({ url, title, description }) => (
   </Box>
 )
 
-const EmbeddedPdf = ({ description, title, url, width, height }) => (
+const EmbeddedEmbed = ({ description, title, url, width, height }) => (
   <Box margin={{ bottom: "medium" }}>
     <Description description={description} />
-    <PdfWrapper ratio={Math.round((height / width) * 100)}>
-      <Pdf name={title} type="application/pdf" src={`https:${url}`} />
-    </PdfWrapper>
+    <EmbedWrapper ratio={Math.round((height / width) * 100)}>
+      <Embed name={title} type="application/pdf" src={url} />
+    </EmbedWrapper>
+  </Box>
+)
+
+const EmbeddedIFrame = ({ description, title, url, width, height }) => (
+  <Box margin={{ bottom: "medium" }}>
+    <Description description={description} />
+    <EmbedWrapper ratio={Math.round((height / width) * 100)}>
+      <IFrame name={title} type="application/pdf" src={url} />
+    </EmbedWrapper>
+  </Box>
+)
+
+const EmbeddedImages = ({ title, columns, images }) => (
+  <Box margin={{ bottom: "medium" }}>
+    <Grid
+      gap="small"
+      columns={{
+        count: columns,
+        size: "auto",
+      }}
+    >
+      {images.map(({ fields }) => (
+        <Image key={fields.title} file={fields.file} alt={title} />
+      ))}
+    </Grid>
   </Box>
 )
 
@@ -110,18 +150,10 @@ const options = {
     [BLOCKS.LIST_ITEM]: (_, children) => <ListItem>{children}</ListItem>,
     [BLOCKS.EMBEDDED_ASSET]: (node) => {
       const { title, file, description } = get(node, "data.target.fields")
-      const { url, contentType, details } = file
+      const { url, contentType } = file
       if (contentType.startsWith("image")) {
-        const { width, height } = details?.image || {}
-
         return (
-          <EmbeddedImage
-            description={description}
-            height={height}
-            title={title}
-            url={url}
-            width={width}
-          />
+          <EmbeddedImage description={description} title={title} file={file} />
         )
       }
 
@@ -134,19 +166,41 @@ const options = {
       return null
     },
     [BLOCKS.EMBEDDED_ENTRY]: (node) => {
-      const { media, height, width, title } = get(node, "data.target.fields")
-      const { file, description } = media.fields
-      const { contentType, url } = file
+      const { fields, sys } = get(node, "data.target")
+      const type = get(sys, "contentType.sys.id")
 
-      if (contentType.endsWith("pdf")) {
+      if (type === "pdf") {
+        const { media, height, width, title } = fields
+        const { file, description } = media.fields
+        const { url } = file
         return (
-          <EmbeddedPdf
-            url={url}
+          <EmbeddedEmbed
+            url={`https:${url}`}
             title={title}
             description={description}
             height={height}
             width={width}
           />
+        )
+      }
+
+      if (type === "youtube") {
+        const { title, url, description } = fields
+        return (
+          <EmbeddedIFrame
+            title={title}
+            url={url}
+            description={description}
+            height={9}
+            width={16}
+          />
+        )
+      }
+
+      if (type === "html") {
+        const { title, images, columns } = fields
+        return (
+          <EmbeddedImages title={title} images={images} columns={columns} />
         )
       }
 
