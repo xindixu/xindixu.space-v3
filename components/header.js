3e27be5a875f4ff5cde7000d819d429e73279bf3
Xindi Xu <xindixu0@gmail.com>
2021-06-18 02:16:37 -0500

    feat(animation): parallex effect for headers

--- components/header.js
+++ components/header.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Box, Heading, Stack } from "grommet"
 import Image from "next/image"
+import { useViewportScroll, motion, useTransform } from "framer-motion"
 import styled, { css } from "styled-components"
 import styleSettings from "lib/style-settings"
 import { media } from "lib/style-settings/media-query"
@@ -22,9 +23,9 @@ const Content = styled(Box)`
   height: ${medium};
 
   ${media.mdUp`
-        width: ${large};
-        height: ${large};
-      `}
+    width: ${large};
+    height: ${large};
+  `}
 
   background: radial-gradient(circle, ${white}55 80%, ${white} 100%);
 `
@@ -48,29 +49,47 @@ const Header = React.forwardRef(
       ? { height: "100vh", width: "100vw" }
       : { height: "50vh", width: "100vw" }
 
+    const { scrollY } = useViewportScroll()
+    const backgroundY = useTransform(
+      scrollY,
+      [0, 400],
+      full ? [0, 200] : [0, 100]
+    )
+    const titleY = useTransform(scrollY, [0, 400], full ? [0, 300] : [0, 150])
+
     return (
       <Wrapper anchor="center" full={full}>
-        <Box {...size} ref={ref}>
-          <Image src={src} layout="fill" priority objectFit="cover" alt={alt} />
-        </Box>
-        <Gradient {...size} />
-        <Content align="center" justify="center">
-          <Heading
-            color="black"
-            size={full ? "xlarge" : "medium"}
-            textAlign="center"
-          >
-            {name}
-          </Heading>
-          <Box direction="row" wrap justify="center">
-            {labels &&
-              labels.map((label) => (
-                <Label key={label} pad={{ horizontal: "small" }} round>
-                  {label}
-                </Label>
-              ))}
+        <motion.div style={{ y: backgroundY, x: 0 }}>
+          <Box {...size} ref={ref}>
+            <Image
+              src={src}
+              layout="fill"
+              priority
+              objectFit="cover"
+              alt={alt}
+            />
           </Box>
-        </Content>
+        </motion.div>
+        <Gradient {...size} />
+        <motion.div style={{ y: titleY, x: 0 }}>
+          <Content align="center" justify="center">
+            <Heading
+              color="black"
+              size={full ? "xlarge" : "medium"}
+              textAlign="center"
+            >
+              {name}
+            </Heading>
+            <Box direction="row" wrap justify="center">
+              {labels &&
+                labels.map((label) => (
+                  <Label key={label} pad={{ horizontal: "small" }} round>
+                    {label}
+                  </Label>
+                ))}
+            </Box>
+          </Content>
+        </motion.div>
       </Wrapper>
     )
   }
