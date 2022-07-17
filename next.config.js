69ffe759a87192f8349cdf651affcf756e28cfb9
Xindi Xu <xindixu0@gmail.com>
2022-07-17 03:33:28 -0400

    tool: ignore js build errors

--- next.config.js
+++ next.config.js
@@ -14,10 +14,13 @@ module.exports = {
     return config
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
+    // FIXME: Dangerously allow production builds to successfully complete even if
     // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    // FIXME: Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 }
